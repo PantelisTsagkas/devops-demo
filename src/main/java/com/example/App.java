7ad0df85f0158b311fdc4fd075d7b83e4686bde6
pantelis <pantelis.tsagkas@jet2.com>
2025-03-14 13:08:08 +0000

    Test CI/CD trigger

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Pantelis and I am learning DevOps!");
+        System.out.println("Hello! My name is Pantelis and I am learning DevOps!!");
     }
 }
